87375ee648819da1a63e722197aee8660c5d1d9d
Daniela Boteva <daniebot@amdocs.com>
2022-05-23 10:26:13 +0300

    DivisibleBy5List problem edited

--- src/com/homework/lecture4/DivisibleBy5List.java
+++ src/com/homework/lecture4/DivisibleBy5List.java
@@ -5,10 +5,10 @@ public class DivisibleBy5List {
         int[] arr = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 5 == 0) {
-                System.out.println(arr[i]);
-                if (arr[i] == 150){
+                if (arr[i] > 150){
                     break;
                 }
+                System.out.println(arr[i]);
             }
 
         }
